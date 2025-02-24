67bca5fa4eefbf97ff6b8d446393d5fa293abe34
Manu <manuel.liebana.2001@fernando3martos.com>
2025-02-24 10:03:32 +0100

    Agregando animaciones

--- lib/presentation/screens/character_screen.dart
+++ lib/presentation/screens/character_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:peaky_blinders_app/provider/character_provider.dart';
 import 'package:peaky_blinders_app/services/character_service.dart';
@@ -28,67 +29,74 @@ class _CharacterView extends StatelessWidget {
       child: CircularProgressIndicator(),
     )
     :
-    ListView.builder(
-      physics: ScrollPhysics(),
-      itemCount: provider.characters.length,
-      itemBuilder: (context, index) {
-        provider.characters[index].heroId = 'hero-${provider.characters[index].id}';
-        return GestureDetector(
-          onTap: () => Navigator.pushNamed(context, 'characterDetail',arguments: provider.characters[index]),
-          child: Card(
-            elevation: 4,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            color: Colors.black87,
-            shadowColor: Colors.black45,
-            child: Column(
-              children: [
-                Hero(
-                  tag: provider.characters[index].heroId!,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-                    child: Image.network(
-                      provider.characters[index].imageUrl!,
-                      height: 180,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+    ZoomIn(
+      duration: Duration(seconds: 2),
+      animate: true,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: provider.characters.length,
+        itemBuilder: (context, index) {
+          provider.characters[index].heroId = 'hero-${provider.characters[index].id}';
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'characterDetail',arguments: provider.characters[index]),
+            child: Card(
+              elevation: 4,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              color: Colors.black87,
+              shadowColor: Colors.black45,
+              child: Column(
+                children: [
+                  Hero(
+                    tag: provider.characters[index].heroId!,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                      child: Image.network(
+                        provider.characters[index].imageUrl!,
+                        height: 180,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset('assets/images/loading.gif');
+                        },
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        provider.characters[index].name,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Merriweather', // Estilo clásico
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          provider.characters[index].name,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Merriweather', // Estilo clásico
+                          ),
+                          textAlign: TextAlign.left,
                         ),
-                        textAlign: TextAlign.left,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        provider.characters[index].actor,
-                        style: TextStyle(
-                          color: Colors.grey[400],
-                          fontSize: 16,
-                          fontFamily: 'Merriweather',
+                        SizedBox(height: 8),
+                        Text(
+                          provider.characters[index].actor,
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            fontSize: 16,
+                            fontFamily: 'Merriweather',
+                          ),
+                          textAlign: TextAlign.left,
                         ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
--- lib/presentation/screens/episodes_list_screen.dart
+++ lib/presentation/screens/episodes_list_screen.dart
@@ -28,11 +28,13 @@ class EpisodesListScreen extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       body: PageView.builder(
-        physics: ScrollPhysics(),
+        physics: BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         onPageChanged: (value) {
-          if (provider.season == 6) provider.changeSeason(1);
-          else provider.changeSeason(provider.season + 1);
+          if (provider.season == 6)
+            provider.changeSeason(1);
+          else
+            provider.changeSeason(provider.season + 1);
         },
         itemBuilder: (context, index) {
           return _EpisodeListView(season: provider.season);
@@ -54,36 +56,62 @@ class _EpisodeListView extends StatelessWidget {
     return ListView.builder(
       itemCount: provider.aux.length,
       itemBuilder: (context, index) {
-        return ExpansionTile(
-          onExpansionChanged: (value) => provider.changeExpansion(value),
-          trailing: Icon(
-            !provider.expansionChange
-                ? Icons.arrow_back_ios
-                : Icons.arrow_downward,
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
             color: Colors.black87,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black54,
+                offset: Offset(4, 4),
+                blurRadius: 6,
+              ),
+            ],
           ),
-          title: Text(
-            provider.aux[index].title,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Serif',
-              fontSize: 18,
+          child: ExpansionTile(
+            expansionAnimationStyle: AnimationStyle(
+              duration: Duration(milliseconds: 800),
+              curve: Curves.fastLinearToSlowEaseIn,
+              reverseCurve: Curves.fastEaseInToSlowEaseOut,
+              reverseDuration: Duration(milliseconds: 800),
             ),
-          ),
-          subtitle: Text(
-            'Episode: ${provider.aux[index].episodeNumber} - Season: ${provider.aux[index].seasonNumber}',
-            style: TextStyle(color: Colors.black54),
-          ),
-          leading: Icon(Icons.movie_creation_outlined, color: Colors.black87),
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                provider.aux[index].description,
-                style: TextStyle(fontSize: 16, color: Colors.black87),
+            backgroundColor: Colors.black,
+            collapsedBackgroundColor: Colors.grey[800],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            onExpansionChanged: (value) => provider.changeExpansion(value),
+            trailing: Icon(
+              !provider.expansionChange
+                  ? Icons.arrow_drop_down
+                  : Icons.arrow_drop_up,
+              color: Colors.white70,
+            ),
+            title: Text(
+              provider.aux[index].title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'OldStandardTT', // Fuente más vintage
+                fontSize: 18,
+                color: Colors.white,
               ),
             ),
-          ],
+            subtitle: Text(
+              'Episode: ${provider.aux[index].episodeNumber} - Season: ${provider.aux[index].seasonNumber}',
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+            leading: Icon(Icons.movie_creation_outlined, color: Colors.white70),
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  provider.aux[index].description,
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
